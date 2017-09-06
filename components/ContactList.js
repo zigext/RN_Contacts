7b752c6b90b9c71d1ba4dc7a765fdc6c09dbe78b
zigext <zigext@gmail.com>
2017-09-06 21:44:54 +0700

    change listview to FlatList

--- components/ContactList.js
+++ components/ContactList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ListView, TouchableHighlight, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, ListView, TouchableHighlight, ScrollView, FlatList } from 'react-native';
 import ContactInfo from './ContactInfo'
 import firebase from '../Firebase'
 
@@ -9,59 +9,70 @@ export default class ContactList extends React.Component {
     constructor(props) {
         super(props)
         this.ref = null
-        this.listView = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2,
-        })
+        // this.listView = new ListView.DataSource({
+        //     rowHasChanged: (r1, r2) => r1 !== r2,
+        // })
 
         this.state = {
-            contacts: this.listView.cloneWithRows({}),
+            // contacts: this.listView.cloneWithRows({}),
+            contactsArray: []
+
         }
 
         // Keep a local reference of the TODO items
         this.contacts = {}
+        this.contactsArray = []
+        
     }
 
     getContactsFromFirebase = (uid) => {
-        console.log("YYY ", uid)
         this.ref = firebase.database().ref(`contacts/${uid}`)
         this.ref.on('value', this.handleContactsUpdate)
     }
 
     // Load the contacts on mount
     componentDidMount() {
-        console.log("XXXX ", this.props.uid)
         this.getContactsFromFirebase(this.props.uid)
     }
 
     // Unsubscribe from the todos on unmount
     componentWillUnmount() {
         if (this.ref) {
-            this.ref.off('value', this.handleToDoUpdate)
+            this.ref.off('value', this.handleContactsUpdate)
         }
     }
 
     // Handle contacts updates
     handleContactsUpdate = (snapshot) => {
         this.contacts = snapshot.val() || {};
-        console.log("ZZZZ ", this.contacts)
-        this.setState({
-            contacts: this.listView.cloneWithRows(this.contacts),
+        this.contactsArray = Object.keys(this.contacts).map((k) => this.contacts[k])
+        console.log("XXX ", this.contactsArray)
+        // this.setState({
+        //     contacts: this.listView.cloneWithRows(this.contacts),
+        // })
+         this.setState({
+            contactsArray: this.contactsArray
         })
     }
 
     // Render a contact row
     renderContacts(rowData) {
+        console.log("ROW ", rowData)
         return (
-            <ContactInfo {...rowData}></ContactInfo>
+            <ContactInfo {...rowData.item}></ContactInfo>
         );
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <ListView
+                {/*<ListView
                     dataSource={this.state.contacts}
                     renderRow={(...args) => this.renderContacts(...args)}
+                />*/}
+                <FlatList
+                    data={this.state.contactsArray}
+                    renderItem={this.renderContacts}
                 />
             </View>
         )
